6100313c9be70cff4b461633b5bac897cfa29f3d
creaoy <jk@propeople.com.ua>
2013-02-01 13:33:38 -0800

    adding javascript on show and hide

--- sneaky_panel_menu.js
+++ sneaky_panel_menu.js
@@ -1,13 +1,21 @@
 (function(jQuery) {
-    Drupal.behaviors.sneaky_panel_menu = function() {
-      jQuery('.sneaky-item a').hover(function() {
-          jQuery(this).next().show();
-        }
-        );
-    };
+    Drupal.behaviors.sneaky_panel_menu = {};
     Drupal.behaviors.sneaky_panel_menu.attach = function(context, settings) {
         if (jQuery('.sneaky-wrapper .sneaky-custom-grid p > a div').length == 0) {
             jQuery('.sneaky-wrapper .sneaky-custom-grid p > a').not('a.ctools-dropdown-link').addClass('sneaky-custom-link').prepend('<span></span>');
+        }
+        jQuery('.sneaky-item a').hover(function() {
+          jQuery(this).next().show('fast');
+        },
+        function () {
+          jQuery(this).next().show();
+        });
+
+        jQuery('.sneaky-panel').hover(function() {
+          jQuery(this).show();
+        },
+        function() {
+          jQuery(this).hide();
         }
     }
 })(jQuery);
