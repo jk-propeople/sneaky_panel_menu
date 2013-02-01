83a96902d80630657643dc2e854afe5d21d5584f
creaoy <jk@propeople.com.ua>
2013-01-31 16:59:13 -0800

    Add js to display

--- sneaky_panel_menu.js
+++ sneaky_panel_menu.js
@@ -1,7 +1,12 @@
 (function(jQuery) {
-    Drupal.behaviors.sneaky_panel = {};
-    Drupal.behaviors.sneaky_panel.attach = function(context, settings) {
-        if ( jQuery('.sneaky-wrapper .sneaky-custom-grid p > a div').length==0){
+    Drupal.behaviors.sneaky_panel_menu = function() {
+      jQuery('.sneaky-item a').hover(function() {
+          jQuery(this).next().show();
+        }
+        );
+    };
+    Drupal.behaviors.sneaky_panel_menu.attach = function(context, settings) {
+        if (jQuery('.sneaky-wrapper .sneaky-custom-grid p > a div').length == 0) {
             jQuery('.sneaky-wrapper .sneaky-custom-grid p > a').not('a.ctools-dropdown-link').addClass('sneaky-custom-link').prepend('<span></span>');
         }
     }
