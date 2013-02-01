770fe07066fa6beeb3a5347f6803aebe51ff0d77
creaoy <jk@propeople.com.ua>
2013-02-01 13:55:16 -0800

    fix minor js errors

--- sneaky_panel_menu.js
+++ sneaky_panel_menu.js
@@ -1,21 +1,21 @@
-(function(jQuery) {
+(function($) {
     Drupal.behaviors.sneaky_panel_menu = {};
     Drupal.behaviors.sneaky_panel_menu.attach = function(context, settings) {
-        if (jQuery('.sneaky-wrapper .sneaky-custom-grid p > a div').length == 0) {
-            jQuery('.sneaky-wrapper .sneaky-custom-grid p > a').not('a.ctools-dropdown-link').addClass('sneaky-custom-link').prepend('<span></span>');
+        if ($('.sneaky-wrapper .sneaky-custom-grid p > a div').length == 0) {
+            $('.sneaky-wrapper .sneaky-custom-grid p > a').not('a.ctools-dropdown-link').addClass('sneaky-custom-link').prepend('<span></span>');
         }
-        jQuery('.sneaky-item a').hover(function() {
-          jQuery(this).next().show('fast');
+        $('.sneaky-item a').hover(function() {
+          $(this).next().show('fast');
         },
         function () {
-          jQuery(this).next().show();
+          $(this).next().hide();
         });
 
-        jQuery('.sneaky-panel').hover(function() {
-          jQuery(this).show();
+        $('.sneaky-panel').hover(function() {
+          $(this).show();
         },
         function() {
-          jQuery(this).hide();
-        }
+          $(this).hide();
+        });
     }
 })(jQuery);
